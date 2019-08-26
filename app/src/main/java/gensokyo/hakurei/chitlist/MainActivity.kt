a6dbca586763d4b0e95a2e88e33eacd6b3f14d68
hakurei-raymoo <hakurei.raymoo@gmail.com>
2019-08-26 15:15:33 +0930

    Added nav drawer.

--- app/src/main/java/gensokyo/hakurei/chitlist/MainActivity.kt
+++ app/src/main/java/gensokyo/hakurei/chitlist/MainActivity.kt
@@ -2,11 +2,24 @@ package gensokyo.hakurei.chitlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import gensokyo.hakurei.chitlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // Set up Android Jetpack Navigation
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // Enable nav drawer
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
     }
 }
